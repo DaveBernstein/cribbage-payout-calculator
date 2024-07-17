c83385a7ef1198b1ff02597f63571305fc81f316
dbernstein <dave.bernstein@emberex.com>
2024-07-17 10:32:20 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from "react";
+import "./App.css";
+import PayoutGrid from "./PayoutGrid";
 
 function App() {
+  const [numberOfPlayers, setNumberOfPlayers] = useState(20);
+  const [buyIn, setBuyIn] = useState(3);
+  const [cashPayout, setCashPayout] = useState(3);
+  const [fund28, setFund28] = useState(8);
+
+  const handleChangeNumberOfPlayers = (event) => {
+    setNumberOfPlayers(Number(event.target.valueAsNumber));
+  };
+
+  const handleChangeBuyIn = (event) => {
+    setBuyIn(Number(event.target.valueAsNumber));
+  };
+
+  const handleChangeCashPayout = (event) => {
+    setCashPayout(Number(event.target.valueAsNumber));
+  };
+
+  const handleChangeFund28 = (event) => {
+    setFund28(Number(event.target.valueAsNumber));
+  };
+
+  const getNumberOfPayedPlayers = useCallback((totalPlayerCount) => {
+    return Math.max(Math.min(Math.floor(totalPlayerCount / 5), 10), 3);
+  }, []);
+
+  const totalEntryFees = numberOfPlayers * buyIn;
+  const raffleFund = numberOfPlayers * 0.25;
+  const totalGiftCardAmount = totalEntryFees - cashPayout - raffleFund - fund28;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-content">
+        <h1>Cribbage Payout Calculator</h1>
+        <div className="inputs">
+          <label>
+            Number of Players:
+            <input
+              type="number"
+              inputmode="decimal"
+              value={numberOfPlayers}
+              onChange={handleChangeNumberOfPlayers}
+              max={50}
+              min={6}
+            />
+          </label>
+          <label>
+            Buy-In ($):
+            <input
+              type="number"
+              inputmode="decimal"
+              value={buyIn}
+              onChange={handleChangeBuyIn}
+            />
+          </label>
+          <label>
+            Cash Payout ($):
+            <input
+              type="number"
+              inputmode="decimal"
+              value={cashPayout}
+              onChange={handleChangeCashPayout}
+            />
+          </label>
+          <label>
+            28/29 Fund ($):
+            <input
+              type="number"
+              inputmode="decimal"
+              value={fund28}
+              onChange={handleChangeFund28}
+            />
+          </label>
+        </div>
+        <div className="totalAmounts">
+          <div>Total Entry Fees: ${totalEntryFees}</div>
+          <div>Raffle Fund: ${raffleFund}</div>
+          <div>Total Gift Card Amount: ${totalGiftCardAmount}</div>
+          <div>
+            Players Payed Out: {getNumberOfPayedPlayers(numberOfPlayers)}
+          </div>
+        </div>
+        <PayoutGrid
+          cashPayout={cashPayout}
+          totalGiftCardAmount={totalGiftCardAmount}
+          numberOfPlayers={numberOfPlayers}
+          payedPlayers={getNumberOfPayedPlayers(numberOfPlayers)}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/PayoutGrid.js
@@ -0,0 +1,126 @@
+import React from "react";
+
+const PayoutGrid = ({
+  cashPayout,
+  totalGiftCardAmount,
+  numberOfPlayers,
+  payedPlayers,
+}) => {
+  // last player always gets the special cash payout value, so counts look off by 1
+  const paysByPayedPlayerCount = {
+    3: {
+      1: 0.65,
+      2: 0.35,
+    },
+    4: {
+      1: 0.55,
+      2: 0.3,
+      3: 0.14,
+    },
+    5: {
+      1: 0.48,
+      2: 0.27,
+      3: 0.15,
+      4: 0.1,
+    },
+    6: {
+      1: 0.4,
+      2: 0.26,
+      3: 0.15,
+      4: 0.12,
+      5: 0.07,
+    },
+    7: {
+      1: 0.34,
+      2: 0.24,
+      3: 0.15,
+      4: 0.12,
+      5: 0.09,
+      6: 0.06,
+    },
+    8: {
+      1: 0.29,
+      2: 0.22,
+      3: 0.15,
+      4: 0.12,
+      5: 0.09,
+      6: 0.08,
+      7: 0.05,
+    },
+    9: {
+      1: 0.25,
+      2: 0.2,
+      3: 0.14,
+      4: 0.12,
+      5: 0.09,
+      6: 0.08,
+      7: 0.07,
+      8: 0.05,
+    },
+    10: {
+      1: 0.23,
+      2: 0.19,
+      3: 0.14,
+      4: 0.11,
+      5: 0.09,
+      6: 0.08,
+      7: 0.07,
+      8: 0.06,
+      9: 0.03,
+    },
+  };
+
+  const getOrdinal = (number) => {
+    if (number === 1) {
+      return "st";
+    }
+    if (number === 2) {
+      return "nd";
+    }
+    if (number === 3) {
+      return "rd";
+    }
+    return "th";
+  };
+
+  const getIndividualPayout = (place) => {
+    if (place === payedPlayers) {
+      return `${cashPayout}(cash)`;
+    }
+
+    const paysForCurrentCount = paysByPayedPlayerCount[payedPlayers];
+    const scalingFactorForCurrentPlace = paysForCurrentCount[place];
+
+    return (totalGiftCardAmount * scalingFactorForCurrentPlace).toFixed(2);
+  };
+
+  const placePayoutRows = [];
+  console.log("payedPlayers => ", payedPlayers);
+  for (let i = 1; i <= payedPlayers; i++) {
+    placePayoutRows.push(
+      <tr key={i} className={i % 2 === 0 ? "even-row" : "odd-row"}>
+        <td>
+          {i}
+          {getOrdinal(i)}
+        </td>
+        <td>$ {getIndividualPayout(i)}</td>
+      </tr>
+    );
+  }
+
+  return (
+    <div className="grid-container">
+      <table className="place-payout-table">
+        <thead>
+          <tr>
+            <th>Place</th>
+            <th>Payout</th>
+          </tr>
+        </thead>
+        <tbody>{placePayoutRows}</tbody>
+      </table>
+    </div>
+  );
+};
+
+export default PayoutGrid;
